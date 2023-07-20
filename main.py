128a7d40558e9b9e99796aacd01bb875f88085fd
Giovanni Barros <giovannibarros@hotmail.com>
2023-07-20 20:00:23 -0300

    add logic for the timer and start button

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ work intervals. Each work interval is called a pomodoro, the Italian word for to
 
 #  IMPORTS  #
 import tkinter
+import math
 
 #  CONSTANTS  #
 # colors
@@ -23,18 +24,65 @@ BT_FONT_SIZE = 18
 CHK_FONT_NAME = "Courier"
 CHK_FONT_SIZE = 24
 # timer
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 0.2
+SHORT_BREAK_MIN = 0.1
+LONG_BREAK_MIN = 1
 
 CHECK_MARK = "✔"
-check_mark_txt = ""
+loop_number = 0
 
 #  TIMER RESET  #
 
 #  TIMER MECHANISM  #
 
+
+def start_timer():
+    '''Start the count down, when the start button is clicked.'''
+
+    global loop_number
+
+    work_sec = int(WORK_MIN * 60)
+    short_break_sec = int(SHORT_BREAK_MIN * 60)
+    long_break_sec = int(LONG_BREAK_MIN * 60)
+
+    loop_number += 1
+    worked_times = math.ceil(loop_number / 2)
+
+    if loop_number in [1, 3, 5, 7]:
+        title_label.configure(text="Work")
+        checkmark_label.configure(text=CHECK_MARK*worked_times)
+        count_down(work_sec)
+
+    elif loop_number in [2, 4, 6]:
+        title_label.configure(text="Break")
+        count_down(short_break_sec)
+
+    elif loop_number >= 8:
+        title_label.configure(text="Break")
+        count_down(long_break_sec)
+        loop_number = 0
+
+
 #  COUNTDOWN MECHANISM  #
+def count_down(count):
+    '''
+    Change the displayed time every 1 second.
+
+    Args:
+        count (int): starting number, in seconds.
+    Returns:
+        None.
+    '''
+
+    count_min = f"{count // 60}".zfill(2)
+    count_sec = f"{count % 60}".zfill(2)
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
+    if count > 0:
+        window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+
 
 #  UI SETUP  #
 window = tkinter.Tk()
@@ -48,15 +96,17 @@ title_label = tkinter.Label(text="Timer", bg=YELLOW,
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 132, text="00:00", font=(FONT_NAME, FONT_SIZE, "bold"))
+timer_text = canvas.create_text(
+    100, 132, text="00:00", font=(FONT_NAME, FONT_SIZE, "bold"))
 
 start_button = tkinter.Button(
-    text="Start", font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"))
+    text="Start", highlightthickness=0, font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"),
+    command=start_timer)
 reset_button = tkinter.Button(
-    text="Reset", font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"))
+    text="Reset", highlightthickness=0, font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"))
 
 checkmark_label = tkinter.Label(
-    text=f"{check_mark_txt}", bg=YELLOW, fg=GREEN, font=(CHK_FONT_NAME, CHK_FONT_SIZE, "bold"))
+    text="", bg=YELLOW, fg=GREEN, font=(CHK_FONT_NAME, CHK_FONT_SIZE, "bold"))
 
 
 title_label.grid(row=0, column=1)
@@ -64,5 +114,6 @@ canvas.grid(row=1, column=1)
 start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=2)
 checkmark_label.grid(row=3, column=1)
+
 
 window.mainloop()
