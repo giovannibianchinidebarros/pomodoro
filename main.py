deee4d1d51b6746549a6e03df97613185ee5a1b9
Giovanni Barros <giovannibarros@hotmail.com>
2023-07-21 00:34:47 -0300

    update timer logic, add checkmarks, add reset button logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-'''Pomodoro APP
+'''
+Pomodoro APP
 
 The Pomodoro Technique is a time management method based on 25-minute stretches 
 of focused work broken by five-minute breaks. 
@@ -24,18 +25,27 @@ BT_FONT_SIZE = 18
 CHK_FONT_NAME = "Courier"
 CHK_FONT_SIZE = 24
 # timer
-WORK_MIN = 0.2
-SHORT_BREAK_MIN = 0.1
-LONG_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 25
 
 CHECK_MARK = "✔"
 loop_number = 0
+timer = None
 
 #  TIMER RESET  #
 
+
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.configure(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmark_label.configure(text="")
+    global loop_number
+    loop_number = 0
+
+
 #  TIMER MECHANISM  #
-
-
 def start_timer():
     '''Start the count down, when the start button is clicked.'''
 
@@ -46,19 +56,19 @@ def start_timer():
     long_break_sec = int(LONG_BREAK_MIN * 60)
 
     loop_number += 1
-    worked_times = math.ceil(loop_number / 2)
 
     if loop_number in [1, 3, 5, 7]:
-        title_label.configure(text="Work")
-        checkmark_label.configure(text=CHECK_MARK*worked_times)
+        title_label.configure(text="Work", fg=GREEN)
+        chk_text = checkmark_label.cget("text")
+        checkmark_label.configure(text=f"{chk_text}o")
         count_down(work_sec)
 
     elif loop_number in [2, 4, 6]:
-        title_label.configure(text="Break")
+        title_label.configure(text="Break", fg=PINK)
         count_down(short_break_sec)
 
     elif loop_number >= 8:
-        title_label.configure(text="Break")
+        title_label.configure(text="Break", fg=RED)
         count_down(long_break_sec)
         loop_number = 0
 
@@ -74,13 +84,17 @@ def count_down(count):
         None.
     '''
 
+    global timer
+
     count_min = f"{count // 60}".zfill(2)
     count_sec = f"{count % 60}".zfill(2)
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
 
     if count > 0:
-        window.after(1000, count_down, count-1)
+        timer = window.after(1000, count_down, count-1)
     else:
+        worked_times = math.ceil(loop_number / 2)
+        checkmark_label.configure(text=CHECK_MARK*worked_times)
         start_timer()
 
 
@@ -103,7 +117,8 @@ start_button = tkinter.Button(
     text="Start", highlightthickness=0, font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"),
     command=start_timer)
 reset_button = tkinter.Button(
-    text="Reset", highlightthickness=0, font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"))
+    text="Reset", highlightthickness=0, font=(BT_FONT_NAME, BT_FONT_SIZE, "bold"),
+    command=reset_timer)
 
 checkmark_label = tkinter.Label(
     text="", bg=YELLOW, fg=GREEN, font=(CHK_FONT_NAME, CHK_FONT_SIZE, "bold"))
